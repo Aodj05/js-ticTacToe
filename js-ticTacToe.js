1a89891f052509f4eb8aaea37a20515bd2a5811c
Aodj05 <aodj05@msn.com>
2022-12-27 14:32:15 -0500

    initial commit

---
+++ js-ticTacToe.js
@@ -0,0 +1,12 @@
+//game board modules
+const gameBoard = (() => {
+
+	const board = ["", "", "", "", "", "", "", "", ""];
+});
+
+const displayController = (() =>{});
+
+//player factory
+const PlayerFactory = (name, piece) => {
+    
+}
